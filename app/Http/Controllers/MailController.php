e1f38d8ea7f4b91f862294abe68b184d2e97e37f
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-02-20 19:07:11 +0100

    command will be tested maildev

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -12,10 +12,10 @@ class MailController extends Controller
     {
         $details = [
           'title' => 'Mail test from recherche 5335',
-          'body' => 'This is a test '
+          'body' => 'This is a second test '
         ];
 
-        Mail::to("recherche5335@gmail.com")->send(new TestMail($details));
+        Mail::to('recherche5335@gmail.com')->send(new TestMail($details));
         return "Email Sent";
     }
 }
